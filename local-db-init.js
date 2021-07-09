e6db540ec53cce298bf345ec3d88b5fed00860df
Lucca La Fonte <luccalafonte97@gmail.com>
2021-07-09 16:02:17 -0300

    Changing init script

--- local-db-init.js
+++ local-db-init.js
@@ -3,14 +3,14 @@ const companiesFixtures = [
     chosenBenefits: ["vr", "vt"],
     name: "International Business Machines Corporation",
     tradingName: "IBM",
-    cnpj: "33.372.251/0126-77",
+    cnpj: "33372251012677", // [BUG?] breaks the model constraint
     address: "Rua Tutoia, 1157",
   },
   {
     chosenBenefits: ["vr", "vt", "gymPass"],
     name: "Flash App Flash Tecnologia e Pagamentos Ltda.",
     tradingName: "Flash App",
-    cnpj: "32.223.020/0001-18",
+    cnpj: "32223020000118", // [BUG?] breaks the model constraint
     address: "Eugenio de Medeiros, 242",
   },
 ];
