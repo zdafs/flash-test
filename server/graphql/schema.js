ad14fe808414ea4e5f0b159ef0d7f52a74c07f85
Lucca La Fonte <luccalafonte97@gmail.com>
2021-07-10 12:49:00 -0300

    Updating graphql schema

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -3,24 +3,35 @@ import apolloServer from "apollo-server";
 const { gql } = apolloServer
 
 export const typeDefs = gql`
-  type Company {
-    id: ID
-    name: String
-    tradingName: String
-    cnpj: String
-    address: String
-    chosenBenefits: [String]
-  }
-
   enum Benefits {
     vr
     vt
     gympass
   }
 
+  type Employee {
+    id: ID
+    name: String
+    phone: String
+    cpf: String
+    givenBenefits: [Benefits]
+  }
+
+  type Company {
+    id: ID
+    name: String
+    tradingName: String
+    cnpj: String
+    address: String
+    chosenBenefits: [Benefits]
+    employees: [Employee]
+  }
+
   type Query {
     getAllCompanies: [Company]
     findCompanyById(id: String!): Company
+    getAllEmployees: [Employee]
+    findEmployeeById(id: String!): Employee
   }
 
   type Mutation {
@@ -31,5 +42,13 @@ export const typeDefs = gql`
       address: String!
       chosenBenefits: [Benefits!]! # [BUG?] permits saving array with null value
     ): Company
+
+    createEmployee(
+      name: String!
+      phone: String!
+      cpf: String!
+      givenBenefits: [Benefits!]!
+      companies: [String!]!
+    ): Employee
   }
 `;
